044db2bc3c569de0df363367bb0a80bfc662174b
vikramaditya <vikramaditya91@gmail.com>
2020-07-11 12:00:56 +0200

    set aiohttp version

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ setup(
     install_requires=[
         'requests', 'six', 'pyOpenSSL', 'service-identity', 'dateparser', 'urllib3', 'chardet', 'certifi',
         'cryptography', 
-        'aiohttp==3.6.2',
+        'aiohttp>3.5.0,<=3.7.0',
         'websockets'
     ],
     keywords='binance exchange rest api bitcoin ethereum btc eth neo',
